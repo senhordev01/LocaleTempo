659c1a1c8082c2572029f1985cb31312ee73f86d
Rogerio <contaprogramacao230@gmail.com>
2026-04-14 16:02:17 -0300

    Segundo Commit

--- Projeto1/Pagina_Inicial/index.js
+++ Projeto1/Pagina_Inicial/index.js
@@ -1,20 +1,34 @@
 import { useState } from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
+import Lua from "../assets/lua.png";
 
 export default function Home(){
-    const[tema, setTema]=useState("white")
+    const[tema, setTema]=useState("white");
 
     function Tema_Escuro(){
-        setTema("black");
+        setTema("black");   
     }
+    
+    function Tema_Claro(){
+        setTema("white");
+    }
+    
     return(
         <>
             <View style={{backgroundColor:tema, flex:1}}>
-                <View style={estilo.NavBar}></View>
+                <View style={estilo.NavBar}>
                 {/* <Button onPress={Tema_Escuro} title="Tema Escuro"></Button> */}
-                <TouchableOpacity onPress={Tema_Escuro} style={estilo.Botao}>
-                    <Text>Tema Escuro</Text>
-                </TouchableOpacity>
+                    {tema === "white"?(
+                        <TouchableOpacity onPress={Tema_Escuro} style={estilo.Botao}>
+                            <Text style={estilo.Texto}>Tema Escuro</Text>
+                        </TouchableOpacity>
+                    ):(
+                        <TouchableOpacity onPress={Tema_Claro} style={estilo.Botao}>
+                            <Text style={estilo.Texto}>Tema Claro</Text>
+                        </TouchableOpacity>
+                    )}
+                    {/* <Image source={Lua} style={{width:40, height:40}} /> */}
+                </View>
             </View>
         </>
     );
@@ -27,11 +41,24 @@ const estilo = StyleSheet.create({
         top:0,
         backgroundColor:"blue",
         width:"100%",
-        height:80
+        height:80,
+        flex:1
+
     },
     Botao:{
-        width:100,
-        height:50,
-        backgroundColor:"red"
+        width:200,
+        height:70,
+        backgroundColor:"red",
+        borderRadius:50,
+        alignSelf:"flex-end",
+        marginTop:5
+        
+    },
+    Texto:{
+        color:"white",
+        fontSize:20,
+        fontWeight:"bold",
+        textAlign:"center",
+        margin:13
     }
 });
